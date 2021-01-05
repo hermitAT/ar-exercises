febf884e5d7a030825a6e7279eb4c7d42f25ced4
hermitAT <ahthornev2@gmail.com>
2021-01-04 20:31:42 -0800

    Add one-to-many logic and create records for employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Adam", last_name: "Thorne", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Marley", hourly_rate: 60)
+@store1.employees.create(first_name: "Steve", last_name: "Jobs", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Daniel", last_name: "Sedin", hourly_rate: 60)
+@store2.employees.create(first_name: "Henrik", last_name: "Sedin", hourly_rate: 60)
+@store2.employees.create(first_name: "Roberto", last_name: "Luongo", hourly_rate: 60)
+@store2.employees.create(first_name: "Dan", last_name: "Cloutier", hourly_rate: 60)
